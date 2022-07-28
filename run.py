34a083210b972de405f60498dc0e60c056fb9b6d
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-28 14:31:46 +0000

    Created sales_data function to get datat string from user

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+
+def get_sales_data():
+    print("Enter the sales data (6 numbers) separated by commas, like 10,20,30,40,50,60 etc\n")
+    data_str=input("Enter your data here")
+    print(f"The datat entered is {data_str}")
+
+get_sales_data()
