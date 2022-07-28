1a600fc7e6cf27a8d6d1f6d34eb78583d23403d4
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-07-28 14:36:10 +0000

    Converted entered data into list

--- run.py
+++ run.py
@@ -15,6 +15,6 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 def get_sales_data():
     print("Enter the sales data (6 numbers) separated by commas, like 10,20,30,40,50,60 etc\n")
     data_str=input("Enter your data here")
-    print(f"The datat entered is {data_str}")
-
+    sales_data=data_str.split(",")
+    print(sales_data)
 get_sales_data()
